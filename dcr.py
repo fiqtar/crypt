2bb32d88665332a1dbcafef090e4e7c9ebfd77e5
mwangi victor <tmvicbvn@gmail.com>
2020-09-09 12:16:39 +0300

    Update dcr.py

--- dcr.py
+++ dcr.py
@@ -1,1 +1,10 @@
-im
+import json
+
+with open('sc_cfgs.json', 'r') as reader:
+    cfg = reader.read()
+    
+def cr():
+    data_link = json.loads(cfg)['link']
+    # cdir /data/
+    # mkdir /id_dcr/
+    # put key, data
