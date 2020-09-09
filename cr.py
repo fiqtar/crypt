0354bb78825c285f1fb70f19e2594679bec24a75
mwangi victor <tmvicbvn@gmail.com>
2020-09-09 12:15:36 +0300

    Update cr.py

--- cr.py
+++ cr.py
@@ -4,7 +4,7 @@ with open('sc_cfgs.json', 'r') as reader:
     cfg = reader.read()
     
 def cr():
-  data_link = json.loads(cfg)['link']
-  # cdir /data/
-  # mkdir /id_cr/
-  # put key, data
+	data_link = json.loads(cfg)['link']
+ 	# cdir /data/
+	# mkdir /id_cr/
+	# put key, data
